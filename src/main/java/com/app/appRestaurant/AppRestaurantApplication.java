0417993d4215a112c3210884761c05a18b211709
Maria Eduarda Barone <duda.barone@outlook.com>
2024-11-12 18:29:28 +0100

    Initial commit

---
+++ src/main/java/com/app/appRestaurant/AppRestaurantApplication.java
@@ -0,0 +1,13 @@
+package com.app.appRestaurant;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppRestaurantApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppRestaurantApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/app/appRestaurant/AppRestaurantApplicationTests.java
@@ -0,0 +1,13 @@
+package com.app.appRestaurant;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppRestaurantApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
